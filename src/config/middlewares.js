5af68a6aee0a8947c2b749a981fa50de70c82761
Prasanth Pushparasah <prasanth.pushparasah@plastif.com>
2021-11-23 14:51:34 +0000

    update

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
+import winston from "winston";
 
 const middlerawes = {
     json: express.json(),
-    urlencoded: express.urlencoded({extended: false})
+    urlencoded: express.urlencoded({extended: false}),
+    morgan: morgan('dev'),
+    // winston: winston()
 };
 
 // example
--- src/modules/User/Controller.js
+++ src/modules/User/Controller.js
@@ -1,4 +1,17 @@
 import ApiError from '../../helpers/ApiError';
+import winston from 'winston';
+
+// const logConfiguration = {
+//     'transports': [
+//         new winston.transports.Console(),
+//         new winston.transports.File({
+//             filename: 'logs/example.log'
+//         })
+//     ]
+// };
+
+// const logger = winston.createLogger(logConfiguration);
+
 
 class UserController {
     #models;
@@ -9,10 +22,21 @@ class UserController {
     login = async (req, res, next) => {
         try {
             // const newUser = await this.#models.User.findOne()
-            if (true)
+            if (false)
                 throw new ApiError('error message', 403);
 
             res.status(200).json('youpi');
+
+            // // Log a message
+            // logger.log({
+            // // Message to be logged
+            //     message: 'test1',
+
+            // // Level of the message logging
+            //     level: 'info'
+            // });
+            // logger.info('test2');
+    // Log a message
 
         } catch (error) {
             next(error);
